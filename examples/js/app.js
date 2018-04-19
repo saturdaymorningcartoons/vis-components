3d72fad0d277c93e48c0e667b07da3fa107872d1
Nathan Brown <nathan.brown@annalect.com>
2018-04-19 08:20:32 -0400

    Update examples directory

--- examples/js/app.js
+++ examples/js/app.js
@@ -1,23 +1,9 @@
 'use strict';
-import FreqDistModel from '../../models/FreqDist-Model';
-import FreqDistTable from '../../views/FreqDistTable-View';
-import Table from '../../views/Table-View';
+import ClassificationTable from '../../views/Classification-Table';
+import FrequencyModel from '../../models/Frequency-Model';
 
 const d3 = require('../../lib/d3.custom.min.js');
 
-d3.csv('./data/engineering-interest.csv', (error, data) => {
-  if (error) throw error;
+const model = new FrequencyModel(['defectiveDimensions', 'nonDefectiveDimensions'], ['porous', 'nonPorous']);
 
-  d3.text('../templates/table.html', str => {
-    // tag the figure with an identifier
-    d3.select('.container').html(str).select('figure').attr('id', 'first');
-
-    new Table(data, 'first', 'Table 1.1 Survey of Engineering Interest').init();
-
-    d3.select('.container').append('div').html(str).select('figure').attr('id', 'second');
-
-    const model = new FreqDistModel(data).group('group');
-
-    new FreqDistTable(model, 'second', 'group', 'Table 1.2, Participant Totals', 'Strong, E. K., Jr. Nineteen-year followup of engineer interests. J. appl. Psychol., 1952, 36, 65-74.').init();
-  });
-});
+new ClassificationTable('.container', model.init(), ['Porous', 'Non-Porous', 'With defective dimensions', 'Without defective dimensions'], 'Classification of 6805 moulded pieces of vulcanite.<br>Frequencies in per cent.').init();
